f35bb7df4be7e4e73a236f9e3373aa8d32aef446
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-09-07 16:23:24 +0400

    govnokod runner

--- screentemp/screentemp.ino
+++ screentemp/screentemp.ino
@@ -8,6 +8,7 @@
 
 #define RED 6
 #define BLUE 3
+#define BUZZER 8
 
 #define ON HIGH
 #define OFF LOW
@@ -18,12 +19,16 @@
 //http://wiki.amperka.ru/схемы-подключения:подключение-текстового-экрана
 LiquidCrystalRus lcd(4, 5, 10, 11, 12, 13);
 
+int incomingByte = 0;   // for incoming serial data
+
 void setup()
 {
   pinMode(RED, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   lcd.begin(20, 4);
   lcd.clear();
+  Serial.begin(9600);
 }
 
 void loop()
@@ -35,9 +40,26 @@ void loop()
   float r1 = voltage / (VIN - voltage);
   float temperature = 1./( 1./(TERMIST_B)*log(r1)+1./(25. + 273.) ) - 273;
 
-  lcd.clear();
-  lcd.print("Температура: ");
-  lcd.print(temperature);
+//  lcd.clear();
+//  lcd.print("Температура: ");
+//  lcd.print(temperature);
+
+  // pin, freq, duration
+//  tone(BUZZER, 3500, 2000);
+//  delay(2000);
+//  noTone(BUZZER);
+
+
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+  
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    lcd.print(incomingByte);
+  }
 
   if (temperature <= COLD)
   {
