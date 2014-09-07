d3dfc2c35883f57cc11e32de894e902988109362
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-09-07 12:55:36 +0400

    diods

--- screentemp/screentemp.ino
+++ screentemp/screentemp.ino
@@ -5,18 +5,24 @@
 
 #define TERMIST_B 4300 
 #define VIN 5.0
+#define RED 6
+#define BLUE 3
 
 //http://wiki.amperka.ru/схемы-подключения:подключение-текстового-экрана
 LiquidCrystalRus lcd(4, 5, 10, 11, 12, 13);
 
 void setup()
 {
+  pinMode(RED, OUTPUT);
+  pinMode(BLUE, OUTPUT);
   lcd.begin(20, 4);
   lcd.clear();
 }
 
 void loop()
 {
+   digitalWrite(RED, HIGH);
+   digitalWrite(BLUE, HIGH);
    float voltage = analogRead(A0) * VIN / 1024.0;
    float r1 = voltage / (VIN - voltage);
  
