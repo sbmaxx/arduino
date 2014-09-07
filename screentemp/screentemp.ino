4add7261db09f9e2b3d0e2b73dfdb7d21a81bef1
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-09-07 13:22:05 +0400

    diods

--- screentemp/screentemp.ino
+++ screentemp/screentemp.ino
@@ -5,8 +5,15 @@
 
 #define TERMIST_B 4300 
 #define VIN 5.0
+
 #define RED 6
 #define BLUE 3
+
+#define ON HIGH
+#define OFF LOW
+
+#define HOT 25
+#define COLD 20
 
 //http://wiki.amperka.ru/схемы-подключения:подключение-текстового-экрана
 LiquidCrystalRus lcd(4, 5, 10, 11, 12, 13);
@@ -21,15 +28,33 @@ void setup()
 
 void loop()
 {
-   digitalWrite(RED, HIGH);
-   digitalWrite(BLUE, HIGH);
-   float voltage = analogRead(A0) * VIN / 1024.0;
-   float r1 = voltage / (VIN - voltage);
+  digitalWrite(RED, HIGH);
+  digitalWrite(BLUE, LOW);
+
+  float voltage = analogRead(A0) * VIN / 1024.0;
+  float r1 = voltage / (VIN - voltage);
+  float temperature = 1./( 1./(TERMIST_B)*log(r1)+1./(25. + 273.) ) - 273;
+
+  lcd.clear();
+  lcd.print("Температура: ");
+  lcd.print(temperature);
+
+  if (temperature <= COLD)
+  {
+    digitalWrite(RED, OFF);
+    digitalWrite(BLUE, ON);
+  }
+  else if (temperature >= COLD && temperature <= HOT)
+  {
+    analogWrite(RED, 125);
+    analogWrite(BLUE, 125);
+  }
+  else
+  {
+    digitalWrite(RED, ON);
+    digitalWrite(BLUE, OFF);
+  }
  
- 
-   float temperature = 1./( 1./(TERMIST_B)*log(r1)+1./(25. + 273.) ) - 273;
-   lcd.clear();
-   lcd.print("Температура: ");
-   lcd.print(temperature);
-   delay(1000);
+  delay(10000);
+
 }
